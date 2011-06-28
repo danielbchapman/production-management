8b2a231fd1b62aadc31f901a20f6c23fb5bd51ea
dchapman <dchapman@theactingcompany.org>
2011-06-28 17:54:33 -0400

    Actually defined these placeholder objects, this is the base revision.

--- ejbModule/com/danielbchapman/production/entity/Hospital.java
+++ ejbModule/com/danielbchapman/production/entity/Hospital.java
@@ -1,8 +1,13 @@
 package com.danielbchapman.production.entity;
 
-public class Hospital extends BaseEntity
+import javax.persistence.Entity;
+
+@Entity
+public class Hospital extends ContactableAndAddressable
 {
 	private static final long serialVersionUID = 1L;
+	private String name;
+	
 	public String getName()
 	{
 		return name;
@@ -11,23 +16,4 @@ public class Hospital extends BaseEntity
 	{
 		this.name = name;
 	}
-	public Address getAddress()
-	{
-		return address;
-	}
-	public void setAddress(Address address)
-	{
-		this.address = address;
-	}
-	public String getPhoneNumber()
-	{
-		return phoneNumber;
-	}
-	public void setPhoneNumber(String phoneNumber)
-	{
-		this.phoneNumber = phoneNumber;
-	}
-	private String name;
-	private Address address;
-	private String phoneNumber;
 }
--- ejbModule/com/danielbchapman/production/entity/Venue.java
+++ ejbModule/com/danielbchapman/production/entity/Venue.java
@@ -1,187 +1,46 @@
 package com.danielbchapman.production.entity;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.Lob;
 
 /**
  * A simple representation of a Venue
  * 
  */
 @Entity
-public class Venue extends BaseEntity
+public class Venue extends ContactableAndAddressable
 {
   private static final long serialVersionUID = 1L;
-  
-  private VenueStatistics venueStatistics;
-	@ManyToOne(targetEntity=Company.class)
-	private Company company;
-	@Column(length = 40)
-	private String venueUsername;
-	@Column(length = 40)
-	private String venuePassword;
-	@Column(length = 40)
-	private String venueRole;
 	
-	private Address address;
-	private EmbeddableContactInformation contact;
-	
-	private String production;
-	@Temporal(value = TemporalType.DATE)
-	private Date productionDate;
-	@Temporal(value = TemporalType.TIME)
-	private Date productionTime;
-	private Boolean advanceComplete;
-  @Column(length=256)
-  private String hospitality;
-	
-	@Column(length = 40)
-	private String milageFromPrevious;
-	
+	private City city;
 	@Column(length = 128)
 	private String name;
+	@Column(length=256)
+	private String prosceniumToBackWall;
+	@Column(length=256)
+	private String prosceniumWidth;
+	@Column(length=256)
+	private String prosceniumHeight;
+	@Column(length=256)
+	private String prosceniumToApron;
+	@Column(length=256)
+	private String apronWidth;
+	@Column(length=256)
+	private String washerDryer;
+  @Column(length=256)
+  private String dressingRoom;
+  @Column(length=256)
+  private String loadingDock;
+  @Lob
+  private String notes;  
+  @Column
+  private String houseCapacity;
+	
 	public Venue()
 	{
 		super();
 	}
-
-	public VenueStatistics getVenueStatistics()
-	{
-	  if(venueStatistics == null)
-	    venueStatistics = new VenueStatistics();
-		return venueStatistics;
-	}
-
-	public void setVenueStatistics(VenueStatistics venueStatistics)
-	{
-		this.venueStatistics = venueStatistics;
-	}
-
-  public Company getCompany()
-  {
-    return company;
-  }
-
-  public void setCompany(Company company)
-  {
-    this.company = company;
-  }
-
-  public String getVenueUsername()
-  {
-    return venueUsername;
-  }
-
-  public void setVenueUsername(String venueUsername)
-  {
-    this.venueUsername = venueUsername;
-  }
-
-  public String getVenuePassword()
-  {
-    return venuePassword;
-  }
-
-  public void setVenuePassword(String venuePassword)
-  {
-    this.venuePassword = venuePassword;
-  }
-
-  public String getVenueRole()
-  {
-    return venueRole;
-  }
-
-  public void setVenueRole(String venueRole)
-  {
-    this.venueRole = venueRole;
-  }
-
-  public Address getAddress()
-  {
-    if(address == null)
-      address = new Address();
-    return address;
-  }
-
-  public void setAddress(Address address)
-  {
-    if(address == null)
-      address = new Address();
-    this.address = address;
-  }
-
-  public EmbeddableContactInformation getContact()
-  {
-    if(contact == null)
-      contact = new EmbeddableContactInformation();
-    
-    return contact;
-  }
-
-  public void setContact(EmbeddableContactInformation contact)
-  {
-    if(contact == null)
-      contact = new EmbeddableContactInformation();
-    
-    this.contact = contact;
-  }
-
-  public String getProduction()
-  {
-    return production;
-  }
-
-  public void setProduction(String production)
-  {
-    this.production = production;
-  }
-
-  public Date getProductionDate()
-  {
-    return productionDate;
-  }
-
-  public void setProductionDate(Date productionDate)
-  {
-    this.productionDate = productionDate;
-  }
-
-  public Date getProductionTime()
-  {
-    return productionTime;
-  }
-
-  public void setProductionTime(Date productionTime)
-  {
-    this.productionTime = productionTime;
-  }
-
-  public Boolean getAdvanceComplete()
-  {
-    if(advanceComplete == null)
-      advanceComplete = Boolean.FALSE;
-    return advanceComplete;
-  }
-
-  public void setAdvanceComplete(Boolean advanceComplete)
-  {
-    this.advanceComplete = advanceComplete;
-  }
-
-  public String getMilageFromPrevious()
-  {
-    return milageFromPrevious;
-  }
-
-  public void setMilageFromPrevious(String milageFromPrevious)
-  {
-    this.milageFromPrevious = milageFromPrevious;
-  }
 
   public String getName()
   {
@@ -191,15 +50,5 @@ public class Venue extends BaseEntity
   public void setName(String name)
   {
     this.name = name;
-  }
-
-  public String getHospitality()
-  {
-    return hospitality;
-  }
-
-  public void setHospitality(String hospitality)
-  {
-    this.hospitality = hospitality;
   }
 }
