95ea9f85bb1f9ccb1026eaa7562e1cf585629901
dchapman <dchapman@theactingcompany.org>
2011-06-28 17:54:14 -0400

    Redefined events to parent to "EventMapping" allowing multiple types of events. The increased complexity of the calendar required this change and now these elements can be individually stored.
    This did break the existing printout and will require an EntityManager
    based report instead of straight SQL (a good thing).

--- ejbModule/com/danielbchapman/production/entity/Performance.java
+++ ejbModule/com/danielbchapman/production/entity/Performance.java
@@ -1,41 +1,221 @@
 package com.danielbchapman.production.entity;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
 
 /**
- * A Performance is a particular instance of a Production in a location. The
- * performance is created in the Scheduling and can be modified via a Venu.
+ * A performance is an extension of an event that allows for some other
+ * linked objects to be created. This adds considerable complexity to the 
+ * calendar, but it does provide a way to interact with other parts of the
+ * database.
+ * 
+ * Upon creating an event you can convert it to a performance.
  *
  ***************************************************************************
  * @author Daniel B. Chapman 
- * @since Jun 16, 2011
+ * @since Jun 20, 2011
  * @link http://www.theactingcompany.org
  ***************************************************************************
  */
-public class Performance extends BaseEntity
+@Entity
+public class Performance extends EventMapping
 {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 2L;
+
+  @ManyToOne(cascade=CascadeType.ALL, optional=true, targetEntity=PerformanceAdvance.class)
+  private PerformanceAdvance advance;
+  private PerformanceSchedule performanceSchedule;	
+  private boolean talkback;
+  private boolean crewCall;
+	private boolean strike;
+	private boolean fightCall;
+	private Venue venue;
 	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date performanceTimeUTC;
-	public Date getPerformanceTimeUTC()
+	public Performance()
+  {
+    super();
+  }
+	
+	public PerformanceAdvance getAdvance()
 	{
-		return performanceTimeUTC;
+		return advance;
 	}
-	public void setPerformanceTimeUTC(Date performanceTimeUTC)
+
+	/**
+   * The event sequence is a series of event related to a specific
+   * performance. This includes all the events defaulted in by the 
+   * performance schedule. This method is per-entity and allows 
+   * quick and easy generation of the schedule so that the logic
+   * can be in one place.
+   * 
+   * @return a list of all the events revolving around this performance
+   * <em>excluding the performance itself.</em> If the performance schedule is null it
+   * returns an empty list.  
+   * 
+   */
+  @Transient
+  public ArrayList<PerformanceEvent> getEventSequence()
+  {
+  	ArrayList<PerformanceEvent> events = new ArrayList<PerformanceEvent>();
+  	
+  	if(performanceSchedule == null)
+  		return events;
+  	if(crewCall)
+  		events.add(getEvent("Crew Call", performanceSchedule.getCrewCall(), performanceSchedule.getCrewCall(), true, true, true));
+  	
+  	events.add(getEvent("AEA Call", performanceSchedule.getActorsEquityCall(), 30, true, true, true));
+  	if(fightCall)
+  		events.add(getEvent("Fight Call", performanceSchedule.getFightCall(), 15, true, true, true));
+  	events.add(getEvent("House Open", performanceSchedule.getHouseCall(), performanceSchedule.getHouseCall(), true, true, true));
+  	
+  	if(talkback)
+  	{
+  		events.add(getEvent("Talkback", performanceSchedule.getPerformanceLength(), 15, false, true, true));
+  		if(strike)
+  			events.add(getEvent("Strike", performanceSchedule.getPerformanceLength() + 15, performanceSchedule.getStrikeLength(), false, true, true));
+  	}
+  	else
+  		if(strike)
+  			events.add(getEvent("Strike", performanceSchedule.getPerformanceLength(), performanceSchedule.getStrikeLength(), false, true, true));
+
+  	return events;
+  }
+  
+  public PerformanceSchedule getPerformanceSchedule()
 	{
-		this.performanceTimeUTC = performanceTimeUTC;
+		return performanceSchedule;
 	}
-	public Production getProduction()
+  
+  public Venue getVenue()
 	{
-		return production;
+		return venue;
 	}
-	public void setProduction(Production production)
+
+	public boolean isCrewCall()
 	{
-		this.production = production;
+		return crewCall;
 	}
-	private Production production;
+
+	public boolean isFightCall()
+	{
+		return fightCall;
+	}
+	
+  /**
+   * @return <b>true</b> this is a performance, it returns true   
+   */
+  @Transient 
+  @Override
+  public boolean isPerformance()
+  {
+  	return true;
+  }
+  
+	public boolean isStrike()
+	{
+		return strike;
+	}
+	
+	public boolean isTalkback()
+	{
+		return talkback;
+	}
+	
+	public void setAdvance(PerformanceAdvance advance)
+	{
+		this.advance = advance;
+	}
+
+	public void setCrewCall(boolean crewCall)
+	{
+		this.crewCall = crewCall;
+	}
+	
+	public void setFightCall(boolean fightCall)
+	{
+		this.fightCall = fightCall;
+	}
+	
+	public void setPerformanceSchedule(PerformanceSchedule performanceSchedule)
+	{
+		this.performanceSchedule = performanceSchedule;
+	}
+	
+  public void setStrike(boolean strike)
+	{
+		this.strike = strike;
+	}
+
+	public void setTalkback(boolean talkback)
+	{
+		this.talkback = talkback;
+	}
+
+	public void setVenue(Venue venue)
+	{
+		this.venue = venue;
+	}
+  
+  /*
+   * (non-Javadoc)
+   * @see java.lang.Object#toString()
+   */
+  public String toString()
+  {
+    StringBuilder buf = new StringBuilder();
+
+    buf.append(super.toString());
+    buf.append("' Performance:'");
+    buf.append(performanceSchedule == null ? "null" : performanceSchedule.getName());
+    buf.append("' Venue:'");
+    buf.append(venue == null ? "null" : venue.getName());
+    buf.append("' Advance:'");
+    buf.append(advance);
+
+    return buf.toString();
+  }
+  
+  private final PerformanceEvent getEvent(String description, int offset, int duration, boolean eventBeforePerformance, boolean cast, boolean crew)
+  {
+  	Calendar startTime = Calendar.getInstance();
+  	Calendar endTime = Calendar.getInstance();
+  	startTime.setTime(composeUtcFromDay(getDay(), getStart()));
+  	endTime.setTime(composeUtcFromDay(getDay(), getStart()));
+  	
+  	if(eventBeforePerformance)
+  	{
+    	startTime.add(Calendar.MINUTE, offset * -1);
+    	endTime.add(Calendar.MINUTE, (offset * -1 ) + duration);  		
+  	}
+  	else
+  	{
+    	startTime.add(Calendar.MINUTE, offset);
+    	endTime.add(Calendar.MINUTE, offset + duration);
+  	}
+  	
+  	return new PerformanceEvent(getDay(), description, startTime.getTime(), endTime.getTime(), cast, crew);
+  }
+  
+  public class PerformanceEvent extends EventMapping
+  {
+		private static final long serialVersionUID = 1L;
+		
+		public PerformanceEvent(Day day, String description, Date start, Date end, boolean cast, boolean crew)
+		{
+			setDay(day);
+			setDescription(description);
+			setStart(start);
+			setEnd(end);
+			setCast(cast);
+			setCrew(crew);
+		}
+  }
 }
