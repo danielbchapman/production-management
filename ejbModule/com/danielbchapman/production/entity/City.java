9397d5c102590d75812748ef18444591ba3e4382
dchapman <dchapman@theactingcompany.org>
2011-06-28 17:53:05 -0400

    Redefinition of a city, this is the base instance for the calendars.

--- ejbModule/com/danielbchapman/production/entity/City.java
+++ ejbModule/com/danielbchapman/production/entity/City.java
@@ -2,6 +2,12 @@ package com.danielbchapman.production.entity;
 
 import java.util.Collection;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+
 /**
  * A city, like one that we live in.
  *
@@ -11,17 +17,127 @@ import java.util.Collection;
  * @link http://www.theactingcompany.org
  ***************************************************************************
  */
+@Entity
 public class City extends BaseEntity
 {
 	private static final long serialVersionUID = 1L;
 
-	private String name;
-	private String stateOrTerritory;
 	private String country;
+	@OneToMany(cascade={CascadeType.ALL},targetEntity=Hospital.class, fetch=FetchType.EAGER)
+	private Collection<Hospital> hospitals;
+	@OneToMany(cascade={CascadeType.ALL},targetEntity=Hotel.class, fetch=FetchType.EAGER)
 	private Collection<Hotel> hotels;
+	private String name;
+	private Hotel selectedHotel;
+	private String stateOrTerritory;
+	private String taxiServiceAddress;
 	private String taxiServiceName;
 	private String taxiServicePhone;
-	private String taxiServiceAddress;
-	private Collection<Hospital> hospitals;
-	private Hotel selectedHotel;
+
+	public String getCountry()
+	{
+		return country;
+	}
+	
+	public Collection<Hospital> getHospitals()
+	{
+		return hospitals;
+	}
+	
+	public Collection<Hotel> getHotels()
+	{
+		return hotels;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public Hotel getSelectedHotel()
+	{
+		return selectedHotel;
+	}
+	
+	public String getStateOrTerritory()
+	{
+		return stateOrTerritory;
+	}
+	
+	public String getTaxiServiceAddress()
+	{
+		return taxiServiceAddress;
+	}
+	
+	public String getTaxiServiceName()
+	{
+		return taxiServiceName;
+	}
+	
+	public String getTaxiServicePhone()
+	{
+		return taxiServicePhone;
+	}
+	
+	public void setCountry(String country)
+	{
+		this.country = country;
+	}
+	
+	public void setHospitals(Collection<Hospital> hospitals)
+	{
+		this.hospitals = hospitals;
+	}
+	
+	public void setHotels(Collection<Hotel> hotels)
+	{
+		this.hotels = hotels;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setSelectedHotel(Hotel selectedHotel)
+	{
+		this.selectedHotel = selectedHotel;
+	}
+	
+	public void setStateOrTerritory(String stateOrTerritory)
+	{
+		this.stateOrTerritory = stateOrTerritory;
+	}
+	
+	public void setTaxiServiceAddress(String taxiServiceAddress)
+	{
+		this.taxiServiceAddress = taxiServiceAddress;
+	}
+	
+	public void setTaxiServiceName(String taxiServiceName)
+	{
+		this.taxiServiceName = taxiServiceName;
+	}
+	
+	public void setTaxiServicePhone(String taxiServicePhone)
+	{
+		this.taxiServicePhone = taxiServicePhone;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.danielbchapman.production.entity.BaseEntity#toString()
+	 */
+	public String toString()
+	{
+		return name;
+	}
+	
+	/**
+	 * @return a combination of City + State/Territory  
+	 * 
+	 */
+	public String toStringDetailed()
+	{
+		return name + ", " + stateOrTerritory; 
+	}
 }
