b1b4c9e40e2aee811b57a190b1d68c7b575d02d4
dchapman <dchapman@theactingcompany.org>
2011-07-07 17:14:07 -0400

    Temp merge--Hibernate is a POS.

--- ejbModule/com/danielbchapman/production/entity/Hospital.java
+++ ejbModule/com/danielbchapman/production/entity/Hospital.java
@@ -7,7 +7,16 @@ public class Hospital extends ContactableAndAddressable
 {
 	private static final long serialVersionUID = 1L;
 	private String name;
+	private City city;
 	
+	public City getCity()
+	{
+		return city;
+	}
+	public void setCity(City city)
+	{
+		this.city = city;
+	}
 	public String getName()
 	{
 		return name;
