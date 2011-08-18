56e77dc49cdc56059ec48684aedde940cd943408
dchapman <dchapman@Daniel.actingco.local>
2011-08-18 12:03:44 -0400

    - Changed to a delegate reference of heirarchy instead of reimplementing over and over again.

---
+++ src/main/java/com/danielbchapman/production/beans/DelegateInstance.java
@@ -0,0 +1,25 @@
+package com.danielbchapman.production.beans;
+
+import org.theactingcompany.persistence.AbstractEntityInstance;
+
+/**
+ * The DelegateInstance for Entity management
+ * @author dchapman
+ * @since Aug 18, 2011
+ * @copyright The Acting Company Aug 18, 2011 @link www.theactingcompany.org
+ */
+class DelegateInstance extends AbstractEntityInstance
+{
+
+	@Override
+	protected String getPersistenceUnitId()
+	{
+		return "production";
+	}
+
+	@Override
+	protected boolean isContainerManaged()
+	{
+		return false;
+	}
+}
