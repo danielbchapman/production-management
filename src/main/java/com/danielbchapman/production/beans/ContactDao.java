0cce8acd8aaacb3b9a7290fc82119bddb0bce8b1
dchapman <dchapman@Daniel.actingco.local>
2011-08-23 13:56:48 -0400

    -Refactored removing the plural of the contacts and added some basic lookup by ID methods

--- src/main/java/com/danielbchapman/production/beans/ContactDao.java
+++ src/main/java/com/danielbchapman/production/beans/ContactDao.java
@@ -9,91 +9,146 @@ import com.danielbchapman.production.entity.Contact;
 import com.danielbchapman.production.entity.ContactGroup;
 import com.danielbchapman.production.entity.EntityInstance;
 import com.danielbchapman.production.entity.Season;
-import com.danielbchapman.production.entity.SeasonContacts;
+import com.danielbchapman.production.entity.SeasonContact;
 
 /**
- * A simple Data Access Object that provides simple methods
- * for creating and maintaining contacts as well as generating the reports
- * associated with them.
- ***************************************************************************
- * @author Daniel B. Chapman 
+ * A simple Data Access Object that provides simple methods for creating and maintaining contacts as
+ * well as generating the reports associated with them.
+ *************************************************************************** 
+ * @author Daniel B. Chapman
  * @since Oct 1, 2009 2009
  * @link http://www.theactingcompany.org
- ***************************************************************************
+ *************************************************************************** 
  */
 @Stateless
 public class ContactDao implements ContactDaoRemote
 {
-/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	//  @PersistenceContext
-  EntityManager em = EntityInstance.getEm();
-  
-  /* (non-Javadoc)
-   * @see com.danielbchapman.production.beans.ContactDaoRemote#getContacts()
-   */
-  @SuppressWarnings("unchecked")
+	EntityManager em = EntityInstance.getEm();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getContacts()
+	 */
+	@SuppressWarnings("unchecked")
 	public ArrayList<Contact> getAllContacts()
-  {
-  	return EntityInstance.getResultList("SELECT c FROM Contact c ORDER BY c.contactName", Contact.class);
-  }
-  /* (non-Javadoc)
-	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getContactsForGroup(com.danielbchapman.production.entity.ContactGroup)
+	{
+		return EntityInstance.getResultList("SELECT c FROM Contact c ORDER BY c.contactName",
+				Contact.class);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getContact(java.lang.Long)
+	 */
+	@Override
+	public Contact getContact(Long id)
+	{
+		return EntityInstance.getEm().find(Contact.class, id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getContactGroup(java.lang.Long)
+	 */
+	@Override
+	public ContactGroup getContactGroup(Long id)
+	{
+		return EntityInstance.getEm().find(ContactGroup.class, id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.danielbchapman.production.beans.ContactDaoRemote#getContactsForGroup(com.danielbchapman
+	 * .production.entity.ContactGroup)
 	 */
 	@Override
 	public ArrayList<Contact> getContactsForGroup(ContactGroup group)
 	{
-		return EntityInstance.getResultList("SELECT c FROM Contact c WHERE c.contactGroup = ?1 ORDER BY c.contactName", Contact.class, group);
+		return EntityInstance.getResultList(
+				"SELECT c FROM Contact c WHERE c.contactGroup = ?1 ORDER BY c.contactName", Contact.class,
+				group);
 	}
-  
-  /* (non-Javadoc)
-	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getContactsForSeason(com.danielbchapman.production.entity.Season)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.danielbchapman.production.beans.ContactDaoRemote#getContactsForSeason(com.danielbchapman
+	 * .production.entity.Season)
 	 */
 	@Override
 	public ArrayList<Contact> getContactsForSeason(Season season)
 	{
-		return
-				EntityInstance.getResultList(
-						"SELECT s.contact FROM SeasonContacts s " + 
-						"WHERE s.season = ?1 AND s.contact IS NOT NULL " + 
-						" ORDER BY s.contact.contactGroup.name, s.contact.contactName",
-						Contact.class,
-						season);
+		return EntityInstance.getResultList("SELECT s.contact FROM SeasonContact s "
+				+ "WHERE s.season = ?1 AND s.contact IS NOT NULL "
+				+ " ORDER BY s.contact.contactGroup.name, s.contact.contactName", Contact.class, season);
 	}
-  
-  /* (non-Javadoc)
-   * @see com.danielbchapman.production.beans.ContactDaoRemote#getGroups()
-   */
-  @SuppressWarnings("unchecked")
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getGroups()
+	 */
+	@SuppressWarnings("unchecked")
 	public ArrayList<ContactGroup> getGroups()
-  {
-  	return EntityInstance.getResultList("SELECT g FROM ContactGroup g ORDER BY g.name", ContactGroup.class);
-  }
-  
-	/* (non-Javadoc)
-   * @see com.danielbchapman.production.beans.ContactDaoRemote#saveContact(com.danielbchapman.production.entity.Contact)
-   */
-  public void saveContact(Contact contact)
-  {
-  	EntityInstance.saveObject(contact);
-  }
-	
-	/* (non-Javadoc)
-   * @see com.danielbchapman.production.beans.ContactDaoRemote#saveGroup(com.danielbchapman.production.entity.ContactGroup)
-   */
-  public void saveGroup(ContactGroup group)
-  {
-  	EntityInstance.saveObject(group);
-  }
-  
-	/* (non-Javadoc)
-	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getSeasonContacts(com.danielbchapman.production.entity.Season)
+	{
+		return EntityInstance.getResultList("SELECT g FROM ContactGroup g ORDER BY g.name",
+				ContactGroup.class);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getSeasonContacts(java.lang.Long)
 	 */
 	@Override
-	public ArrayList<SeasonContacts> getSeasonContacts(Season season)
+	public SeasonContact getSeasonContacts(Long id)
 	{
-		return EntityInstance.getResultList("SELECT s FROM SeasonContacts s WHERE s.seaosn = ?1 ORDER BY s.name", SeasonContacts.class, season);
+		return EntityInstance.getEm().find(SeasonContact.class, id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.beans.ContactDaoRemote#getSeasonContacts(com.danielbchapman.
+	 * production.entity.Season)
+	 */
+	@Override
+	public ArrayList<SeasonContact> getSeasonContacts(Season season)
+	{
+		return EntityInstance.getResultList(
+				"SELECT s FROM SeasonContact s WHERE s.seaosn = ?1 ORDER BY s.name", SeasonContact.class,
+				season);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.danielbchapman.production.beans.ContactDaoRemote#saveContact(com.danielbchapman.production
+	 * .entity.Contact)
+	 */
+	public void saveContact(Contact contact)
+	{
+		EntityInstance.saveObject(contact);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.danielbchapman.production.beans.ContactDaoRemote#saveGroup(com.danielbchapman.production
+	 * .entity.ContactGroup)
+	 */
+	public void saveGroup(ContactGroup group)
+	{
+		EntityInstance.saveObject(group);
 	}
 }
--- src/main/java/com/danielbchapman/production/beans/ContactDaoRemote.java
+++ src/main/java/com/danielbchapman/production/beans/ContactDaoRemote.java
@@ -1,6 +1,5 @@
 package com.danielbchapman.production.beans;
 
-
 import java.io.Serializable;
 import java.util.ArrayList;
 
@@ -9,49 +8,76 @@ import javax.ejb.Remote;
 import com.danielbchapman.production.entity.Contact;
 import com.danielbchapman.production.entity.ContactGroup;
 import com.danielbchapman.production.entity.Season;
-import com.danielbchapman.production.entity.SeasonContacts;
+import com.danielbchapman.production.entity.SeasonContact;
 
 @Remote
 public interface ContactDaoRemote extends Serializable
 {
 	static final long serialVersionUID = 1L;
 
-  /**
-   * @return a list of contact groups from the DB ordered by name
-   */
-  public abstract ArrayList<ContactGroup> getGroups();
-  /**
-   * @param season the season to search.
-   * @return a list of all the contacts assigned to this season.
-   */
-  public abstract ArrayList<Contact> getContactsForSeason(Season season);
-  
-  /**
-   * Return a reference to the linking object for the SeasonContacts
-   * @param season
-   * @return a list of all SeasonContacts currently assigned to this season
-   */
-  public abstract ArrayList<SeasonContacts> getSeasonContacts(Season season);
-  /**
-   * @param group the group to search by.
-   * @return a list of contacts for this group.
-   */
-  public abstract ArrayList<Contact> getContactsForGroup(ContactGroup group);
-  /**
-   * Saves a group
-   * @param group
-   */
-  public abstract void saveGroup(ContactGroup group);
+	/**
+	 * @return a list of known contacts
+	 */
+	public abstract ArrayList<Contact> getAllContacts();
 
-  /**
-   * @return a list of known contacts
-   */
-  public abstract ArrayList<Contact> getAllContacts();
+	/**
+	 * @param id
+	 * @return the entity for this ID
+	 */
+	public Contact getContact(Long id);
 
-  /**
-   * Save a contact to the database
-   * @param contact
-   */
-  public abstract void saveContact(Contact contact);
+	/**
+	 * @param id
+	 * @return the entity for this ID
+	 */
+	public ContactGroup getContactGroup(Long id);
+
+	/**
+	 * @param group
+	 *          the group to search by.
+	 * @return a list of contacts for this group.
+	 */
+	public abstract ArrayList<Contact> getContactsForGroup(ContactGroup group);
+
+	/**
+	 * @param season
+	 *          the season to search.
+	 * @return a list of all the contacts assigned to this season.
+	 */
+	public abstract ArrayList<Contact> getContactsForSeason(Season season);
+
+	/**
+	 * @return a list of contact groups from the DB ordered by name
+	 */
+	public abstract ArrayList<ContactGroup> getGroups();
+
+	/**
+	 * @param id
+	 *          the id
+	 * @return return the entity for this ID
+	 */
+	public SeasonContact getSeasonContacts(Long id);
+
+	/**
+	 * Return a reference to the linking object for the SeasonContacts
+	 * 
+	 * @param season
+	 * @return a list of all SeasonContacts currently assigned to this season
+	 */
+	public abstract ArrayList<SeasonContact> getSeasonContacts(Season season);
+
+	/**
+	 * Save a contact to the database
+	 * 
+	 * @param contact
+	 */
+	public abstract void saveContact(Contact contact);
+
+	/**
+	 * Saves a group
+	 * 
+	 * @param group
+	 */
+	public abstract void saveGroup(ContactGroup group);
 
 }
