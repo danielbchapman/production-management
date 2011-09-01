53787311c6d6bc2a290cd18d19fa4f5caf1c4328
dchapman <dchapman@Daniel.actingco.local>
2011-09-01 16:18:41 -0400

    - Added Contacts and Reporting 4.1

---
+++ src/main/java/com/danielbchapman/production/entity/ContactAddress.java
@@ -0,0 +1,180 @@
+package com.danielbchapman.production.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+
+/**
+ * A linked entity that provides one of many addresses for a particular contact.
+ *************************************************************************** 
+ * @author Daniel B. Chapman
+ * @since Jan 22, 2011
+ * @link http://www.theactingcompany.org
+ *************************************************************************** 
+ */
+@Entity
+public class ContactAddress extends BaseEntity
+{
+	private static final long serialVersionUID = 1L;
+	@Column(length = 40)
+	private String city;
+	@ManyToOne(targetEntity = Contact.class, cascade = { CascadeType.REFRESH })
+	private Contact contact;
+	@Column(length = 128)
+	private String label;
+	@Column(length = 128)
+	private String lineOne = "";
+	@Column(length = 128)
+	private String lineTwo = "";
+	@Column(length = 60)
+	private String state;
+	@Column(length = 20)
+	private String zip;
+
+	public String getCity()
+	{
+		return city;
+	}
+
+	/**
+	 * @return the contact
+	 */
+	public Contact getContact()
+	{
+		return contact;
+	}
+
+	/**
+	 * @return the first line of an address in a two line format.
+	 */
+	@Transient
+	public String getFormattedAddressTwoLineA()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(getLineOne());
+		if(getLineTwo() != null && !getLineTwo().isEmpty())
+		{
+			builder.append(", ");
+			builder.append(getLineTwo());
+		}
+
+		return builder.toString();
+	}
+
+	/**
+	 * @return the second line of an address in a two line format.
+	 */
+	@Transient
+	public String getFormattedAddressTwoLineB()
+	{
+		StringBuilder builder = new StringBuilder();
+
+		builder.append(getCity());
+		builder.append(", ");
+		builder.append(getState());
+		builder.append(" ");
+		builder.append(getZip());
+
+		return builder.toString();
+	}
+
+	/**
+	 * @return the label
+	 */
+	public String getLabel()
+	{
+		return label;
+	}
+
+	public String getLineOne()
+	{
+		return lineOne;
+	}
+
+	public String getLineTwo()
+	{
+		return lineTwo;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+	/**
+	 * @param contact
+	 *          the contact to set
+	 */
+	public void setContact(Contact contact)
+	{
+		this.contact = contact;
+	}
+
+	/**
+	 * @param label
+	 *          the label to set
+	 */
+	public void setLabel(String label)
+	{
+		this.label = label;
+	}
+
+	public void setLineOne(String lineOne)
+	{
+		this.lineOne = lineOne;
+	}
+
+	public void setLineTwo(String lineTwo)
+	{
+		this.lineTwo = lineTwo;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.danielbchapman.production.entity.BaseEntity#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("[");
+		builder.append(label);
+		builder.append("] ");
+		builder.append(lineOne);
+		if(lineTwo != null && !lineTwo.isEmpty())
+		{
+			builder.append(", ");
+			builder.append(lineTwo);
+		}
+		builder.append(" ");
+		builder.append(city);
+		builder.append(" ");
+		builder.append(state);
+		builder.append(" ");
+		builder.append(zip);
+		return builder.toString();
+	}
+}
--- src/main/java/com/danielbchapman/production/entity/ContactableAndAddressable.java
+++ src/main/java/com/danielbchapman/production/entity/ContactableAndAddressable.java
@@ -6,221 +6,229 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.Transient;
 
 /**
- * This is a mutant base entity that provides embedded contact and address information.
- * Java does not support multiple inheritance and this provides consistency
- * across the platform.
+ * This is a mutant base entity that provides embedded contact and address information. Java does
+ * not support multiple inheritance and this provides consistency across the platform.
  * 
  * Please forgive this mutant design, it does save time on the Web side. <em> significant 
  * time which warrants this sub par design, sub objects require significant maintenance and
  * cause null pointer issues with the current Primefaces and JSF2.0 models.</em>
- *
+ * 
  * @see Contactable
  * @see Addressable
- ***************************************************************************
- * @author Daniel B. Chapman 
+ *************************************************************************** 
+ * @author Daniel B. Chapman
  * @since Jun 20, 2011
  * @link http://www.theactingcompany.org
- ***************************************************************************
+ *************************************************************************** 
  */
 @MappedSuperclass
 public abstract class ContactableAndAddressable extends BaseEntity
 {
 	private static final long serialVersionUID = 1L;
-	
+
+	@Column(length = 40)
+	private String addressCity;
 	@Column(length = 128)
-  private String cell;
-  @Column(length = 40)
-  private String addressCity;
-  @Column(length = 128)
-  private String contact;
+	private String cell;
 	@Column(length = 128)
-  private String contactName;
+	private String contactName;
 	@Lob
-  private String contactNotes;
-  @Column(length = 128)
-  private String email;
-  @Column(length = 128)
-  private String fax;
-  @Column(length = 128)
-  private String lineOne;
-  @Column(length = 128)
-  private String lineTwo;
-  @Column(length = 128)
-  private String phone;
-  @Column(length = 60)
-  private String state;
-  @Column(length = 20)
-  private String zip;
-  public String getAddressCity()
+	private String contactNotes;
+	@Column(length = 128)
+	private String contactPosition;
+	@Column(length = 128)
+	private String email;
+	@Column(length = 128)
+	private String fax;
+	@Column(length = 128)
+	private String lineOne;
+	@Column(length = 128)
+	private String lineTwo;
+	@Column(length = 128)
+	private String phone;
+	@Column(length = 60)
+	private String state;
+	@Column(length = 20)
+	private String zip;
+
+	public String getAddressCity()
 	{
 		return addressCity;
 	}
-  public String getCell()
-  {
-    return cell;
-  }
-  
-  public String getContact()
-  {
-    return contact;
-  }
-  
-  public String getContactName()
-  {
-    return contactName;
-  }
-  
-  public String getContactNotes()
-  {
-    return contactNotes;
-  }
-  
-  public String getEmail()
-  {
-    return email;
-  }
-  
-  public String getFax()
-  {
-    return fax;
-  }
-  
-  public String getLineOne()
-  {
-    return lineOne;
-  }
-  
-  public String getLineTwo()
-  {
-    return lineTwo;
-  }
-  
-  public String getPhone()
-  {
-    return phone;
-  }
-  
-  public String getState()
-  {
-    return state;
-  }
-  
-  public String getZip()
-  {
-    return zip;
-  }
-  
-  public void setAddressCity(String addressCity)
+
+	@Transient
+	public String getAsHtml()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append("<div>");
+		builder.append("Not implemented");
+		builder.append("</div>");
+		return builder.toString();
+	}
+
+	public String getCell()
+	{
+		return cell;
+	}
+
+	public String getContactName()
+	{
+		return contactName;
+	}
+
+	public String getContactNotes()
+	{
+		return contactNotes;
+	}
+
+	/**
+	 * @return the contactPosition
+	 */
+	public String getContactPosition()
+	{
+		return contactPosition;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public String getFax()
+	{
+		return fax;
+	}
+
+	@Transient
+	public String getFullAddressNewLines()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(lineOne);
+		builder.append('\n');
+
+		if(lineTwo != null && lineTwo.length() > 0)
+		{
+			builder.append(lineTwo);
+			builder.append('\n');
+		}
+
+		builder.append(addressCity);
+		builder.append(' ');
+		builder.append(state);
+		builder.append(' ');
+		builder.append(zip);
+
+		return builder.toString();
+	}
+
+	@Transient
+	public String getFullAddressSpaced()
+	{
+		StringBuilder builder = new StringBuilder();
+		builder.append(lineOne);
+		builder.append(' ');
+
+		if(lineTwo != null && lineTwo.length() > 0)
+		{
+			builder.append(lineTwo);
+			builder.append(' ');
+		}
+
+		builder.append(addressCity);
+		builder.append(' ');
+		builder.append(state);
+		builder.append(' ');
+		builder.append(zip);
+
+		return builder.toString();
+	}
+
+	public String getLineOne()
+	{
+		return lineOne;
+	}
+
+	public String getLineTwo()
+	{
+		return lineTwo;
+	}
+
+	public String getPhone()
+	{
+		return phone;
+	}
+
+	public String getState()
+	{
+		return state;
+	}
+
+	public String getZip()
+	{
+		return zip;
+	}
+
+	public void setAddressCity(String addressCity)
 	{
 		this.addressCity = addressCity;
 	}
-  
-  public void setCell(String cell)
-  {
-    this.cell = cell;
-  }
-  
-  public void setContact(String contact)
-  {
-    this.contact = contact;
-  }
-  
-  public void setContactName(String contactName)
-  {
-    this.contactName = contactName;
-  }
-  
-  public void setContactNotes(String contactNotes)
-  {
-    this.contactNotes = contactNotes;
-  }
-  
-  public void setEmail(String email)
-  {
-    this.email = email;
-  }
-  
-  public void setFax(String fax)
-  {
-    this.fax = fax;
-  }
-  
-  public void setLineOne(String lineOne)
-  {
-    this.lineOne = lineOne;
-  }
-  
-  public void setLineTwo(String lineTwo)
-  {
-    this.lineTwo = lineTwo;
-  }
-  
-  public void setPhone(String phone)
-  {
-    this.phone = phone;
-  }
-  
-  public void setState(String state)
-  {
-    this.state = state;
-  }
-  
-  public void setZip(String zip)
-  {
-    this.zip = zip;
-  }
-  
-  @Transient
-  public String getFullAddressNewLines()
-  {
-  	StringBuilder builder = new StringBuilder();
-  	builder.append(lineOne);
-  	builder.append('\n');
-  	
-  	if(lineTwo != null && lineTwo.length() > 0)
-  	{
-    	builder.append(lineTwo);
-    	builder.append('\n');  		
-  	}
 
-  	builder.append(addressCity);
-  	builder.append(' ');
-  	builder.append(state);
-  	builder.append(' ' );
-  	builder.append(zip);
-  	
-  	return builder.toString();
-  }
-  
-  @Transient
-  public String getFullAddressSpaced()
-  {
-  	StringBuilder builder = new StringBuilder();
-  	builder.append(lineOne);
-  	builder.append(' ');
-  	
-  	if(lineTwo != null && lineTwo.length() > 0)
-  	{
-    	builder.append(lineTwo);
-    	builder.append(' ');  		
-  	}
+	public void setCell(String cell)
+	{
+		this.cell = cell;
+	}
 
-  	builder.append(addressCity);
-  	builder.append(' ');
-  	builder.append(state);
-  	builder.append(' ' );
-  	builder.append(zip);
-  	
-  	return builder.toString();
-  }
-  
-  @Transient
-  public String getAsHtml()
-  {
-  	StringBuilder builder = new StringBuilder();
-  	builder.append("<div>");
-  	builder.append("Not implemented");
-  	builder.append("</div>");
-  	return builder.toString();
-  }
+	public void setContactName(String contactName)
+	{
+		this.contactName = contactName;
+	}
+
+	public void setContactNotes(String contactNotes)
+	{
+		this.contactNotes = contactNotes;
+	}
+
+	/**
+	 * @param contactPosition
+	 *          the contactPosition to set
+	 */
+	public void setContactPosition(String contactPosition)
+	{
+		this.contactPosition = contactPosition;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+
+	public void setFax(String fax)
+	{
+		this.fax = fax;
+	}
+
+	public void setLineOne(String lineOne)
+	{
+		this.lineOne = lineOne;
+	}
+
+	public void setLineTwo(String lineTwo)
+	{
+		this.lineTwo = lineTwo;
+	}
+
+	public void setPhone(String phone)
+	{
+		this.phone = phone;
+	}
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+	public void setZip(String zip)
+	{
+		this.zip = zip;
+	}
 }
