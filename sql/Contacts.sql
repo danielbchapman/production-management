7df260d5fe16135418919ac0321a6617ac34e54f
dchapman.local <dchapman.local@Computer.actingco.local>
2012-09-26 12:32:20 -0400

    - A marker for the SQL used previously in Contacts

---
+++ sql/Contacts.sql
@@ -0,0 +1,46 @@
+SELECT "id" FROM (SELECT 
+	sc.id AS "id", 
+	g.name AS "name", 
+	c.subGroup AS "subGroup", 
+	c.lastName AS "lastName", 
+	c.firstName AS "firstName" 
+FROM	
+	SeasonContact sc	
+	INNER JOIN Contact c	
+		ON	
+			sc.baseContact_id = c.id 
+	INNER JOIN ContactGroup g 
+		ON	
+			c.contactGroup_id = g.id 
+	WHERE 
+				sc.baseContact_id IS NOT NULL 
+		AND sc.season_id = 3251 
+		 
+UNION ALL 
+ 
+SELECT 
+	sc.id AS "id", 
+	g.name AS "name", 
+	l.subGroup AS "subGroup", 
+	c.lastName AS "lastName", 
+	c.firstName AS "firstName" 
+FROM	
+	SeasonContact sc	
+	INNER JOIN LinkedContact l 
+		ON 
+			sc.linkedContact_id = l.id 
+	INNER JOIN Contact c 
+		ON	
+			l.contact_id = c.id 
+	INNER JOIN ContactGroup g 
+		ON	
+			l.contactGroup_id = g.id 
+	WHERE 
+				sc.linkedContact_id IS NOT NULL 
+		AND sc.season_id = 3251
+ORDER BY	
+	"name", 
+	"subGroup", 
+	"lastName", 
+	"firstName" 
+	) as DerivedForMySql 
