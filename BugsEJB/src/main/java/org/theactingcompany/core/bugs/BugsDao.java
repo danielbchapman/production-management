ded77820c243b918f1c0bd0a953c1836e8861128
danielbchapman <chapman@danielbchapman.com>
2014-11-06 18:10:23 -0600

    - Moved  BugsEJB into the project

---
+++ BugsEJB/src/main/java/org/theactingcompany/core/bugs/BugsDao.java
@@ -0,0 +1,85 @@
+package org.theactingcompany.core.bugs;
+
+import java.util.ArrayList;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class BugsDao
+ */
+@Stateless
+public class BugsDao implements BugsDaoRemote
+{
+
+	/**
+	 * Default constructor.
+	 */
+	public BugsDao()
+	{
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#saveBug(org.theactingcompany.core.bugs.Bug)
+	 */
+	@Override
+	public void saveBug(Bug b)
+	{
+		EntityInstance.saveObject(b);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#resolveBug(org.theactingcompany.core.bugs.Bug, java.lang.String)
+	 */
+	@Override
+	public void resolveBug(Bug b, String reason)
+	{
+		b.setStatus("Resolved: " + reason);
+		b.setResolved(true);
+		EntityInstance.saveObject(b);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#getAllBugs()
+	 */
+	@Override
+	public ArrayList<Bug> getAllBugs()
+	{
+		return EntityInstance.getResultList("SELECT b FROM Bug b", Bug.class);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#getActiveBugs()
+	 */
+	@Override
+	public ArrayList<Bug> getActiveBugs()
+	{
+		return EntityInstance.getResultList("SELECT b FROM Bug b WHERE b.resolved = ?1", Bug.class, Boolean.FALSE);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#getResolvedBugs()
+	 */
+	@Override
+	public ArrayList<Bug> getResolvedBugs()
+	{
+		return EntityInstance.getResultList("SELECT b FROM Bug b WHERE b.resolved = ?1", Bug.class, Boolean.TRUE);
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#submitToJira(org.theactingcompany.core.bugs.Bug)
+	 */
+	public void submitToJira(Bug b)
+	{
+		throw new RuntimeException("Submission to JIRA Is proving harder than I thought, it will be implemented soon depleting the need for this.");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.theactingcompany.core.bugs.BugsDaoRemote#getBug(java.lang.Long)
+	 */
+	@Override
+	public Bug getBug(Long id)
+	{
+		return EntityInstance.getEm().find(Bug.class, id);
+	}
+
+}
